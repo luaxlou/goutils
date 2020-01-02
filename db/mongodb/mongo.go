779b715677b545e3fc3e0858ba77aa1743348992
luaxlou <luax@qq.com>
2020-01-02 17:11:44 +0800

    manual init support

--- db/mongodb/mongo.go
+++ db/mongodb/mongo.go
@@ -12,33 +12,38 @@ var mgo *mongo.Client
 
 var mdb *mongo.Database
 
+func Init() {
+	dsn := os.Getenv("MONGO_DSN")
+	db := os.Getenv("MONGO_DB")
+
+	if dsn == "" {
+		panic("ENV value MONGO_DSN required. example: 'mongodb://root:123456@localhost:27017'")
+
+	}
+
+	if db == "" {
+		panic("ENV value MONGO_DB required.")
+
+	}
+
+	clientOptions := options.Client().ApplyURI(dsn)
+
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	mdb = client.Database(db)
+	mgo = client
+}
+
 func GetDB() *mongo.Database {
 
 	if mgo == nil {
 
-		dsn := os.Getenv("MONGO_DSN")
-		db := os.Getenv("MONGO_DB")
-
-		if dsn == "" {
-			panic("ENV value MONGO_DSN required. example: 'mongodb://root:123456@localhost:27017'")
-
-		}
-
-		if db == "" {
-			panic("ENV value MONGO_DB required.")
-
-		}
-
-		clientOptions := options.Client().ApplyURI(dsn)
-
-		client, err := mongo.Connect(context.Background(), clientOptions)
-
-		if err != nil {
-			panic(err.Error())
-		}
-
-		mdb = client.Database(db)
-		mgo = client
+		//Lazy init
+		Init()
 
 	}
 
