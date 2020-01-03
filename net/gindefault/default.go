bd7defc5bc4d592756cd4a10204fb2ea90053ce3
luaxlou <luax@qq.com>
2020-01-03 16:28:27 +0800

    add gin res

--- net/gindefault/default.go
+++ net/gindefault/default.go
@@ -1,6 +1,7 @@
 package gindefault
 
 import (
+	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -26,4 +27,26 @@ func Run(addr string, initial func(engine *gin.Engine)) {
 	log.Println("Starting http server at address:", addr)
 
 	engine.Run(addr)
+}
+
+
+
+func ReturnSuccess(c *gin.Context, data interface{}) {
+	c.JSON(200, gin.H{
+		"status": 1,
+		"data":   data,
+	})
+
+}
+
+func ReturnFail(c *gin.Context, msg string) {
+
+	fmt.Println(msg)
+	c.JSON(200, gin.H{
+		"status": 0,
+		"msg":    msg,
+	})
+
+	c.Abort()
+
 }
