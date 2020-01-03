c5e650b89c11cbfb395aa7559f786d3a12e861bd
luaxlou <luax@qq.com>
2020-01-03 16:31:14 +0800

    add gin res

--- net/gindefault/default.go
+++ net/gindefault/default.go
@@ -1,7 +1,6 @@
 package gindefault
 
 import (
-	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -27,35 +26,4 @@ func Run(addr string, initial func(engine *gin.Engine)) {
 	log.Println("Starting http server at address:", addr)
 
 	engine.Run(addr)
-}
-
-func ReturnSuccess(c *gin.Context, data interface{}) {
-	c.JSON(200, gin.H{
-		"status": 1,
-		"data":   data,
-	})
-
-}
-
-func ReturnFail(c *gin.Context, msg string) {
-
-	fmt.Println(msg)
-	c.JSON(200, gin.H{
-		"status": 0,
-		"msg":    msg,
-	})
-
-	c.Abort()
-
-}
-func ReturnError(c *gin.Context, err error) {
-
-	fmt.Println(err.Error())
-	c.JSON(200, gin.H{
-		"status": 0,
-		"msg":    err.Error(),
-	})
-
-	c.Abort()
-
 }
---
+++ net/ginutils/utils.go
@@ -0,0 +1,38 @@
+package ginutils
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func ReturnSuccess(c *gin.Context, data interface{}) {
+	c.JSON(200, gin.H{
+		"status": 1,
+		"data":   data,
+	})
+
+}
+
+func ReturnFail(c *gin.Context, msg string) {
+
+	fmt.Println(msg)
+	c.JSON(200, gin.H{
+		"status": 0,
+		"msg":    msg,
+	})
+
+	c.Abort()
+
+}
+func ReturnError(c *gin.Context, err error) {
+
+	fmt.Println(err.Error())
+	c.JSON(200, gin.H{
+		"status": 0,
+		"msg":    err.Error(),
+	})
+
+	c.Abort()
+
+}
+
