82fc1e2cd281d778053e8096fe9b437635858764
luaxlou <luax@qq.com>
2020-02-07 16:39:57 +0800

    envutils

---
+++ tools/envutils/envutils.go
@@ -0,0 +1,42 @@
+package envutils
+
+import "os"
+
+var currEnv = "dev"
+
+const (
+	Prod = "prod"
+	Test = "test"
+	Dev  = "dev"
+)
+
+func init() {
+
+	if env := os.Getenv("APP_ENV"); env != "" {
+
+		SetEnv(env)
+	}
+
+}
+
+func SetEnv(env string) {
+
+	currEnv = env
+
+}
+
+func IsProd() bool {
+	return currEnv == Prod
+
+}
+
+func IsTest() bool {
+	return currEnv == Test
+
+}
+
+func IsDev() bool {
+
+	return currEnv == Dev
+
+}
