c094687bdd107822fcec5740df853fd3cd90e89d
luaxlou <luax@qq.com>
2020-02-14 20:52:35 +0800

    add go mod

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/luaxlou/goutils
+
+go 1.13
+
+require (
+	github.com/gin-contrib/cors v1.3.0
+	github.com/gin-gonic/gin v1.5.0
+	github.com/lionsoul2014/ip2region v2.1.0-release+incompatible
+	github.com/luaxlou/gohttpclient v0.0.0-20190709133742-924eff2205a5
+	go.mongodb.org/mongo-driver v1.3.0
+)
---
+++ wechat/wechatapp/client/client.go
@@ -0,0 +1,63 @@
+package client
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/luaxlou/gohttpclient"
+)
+
+Host = "https://api.weixin.qq.com"
+
+
+type Res struct {
+	Errcode int    `json:"errcode"`
+	Errmsg  string `json:"errmsg"`
+}
+
+type Client struct {
+	appId         string
+	secret        string
+	accessToken   string
+	lastTokenTime time.Time
+}
+
+func New(appId, secret string) *Client {
+
+	c := Client{
+		appId:         appId,
+		secret:        secret,
+		lastTokenTime: time.Now(),
+	}
+
+	return &c
+}
+
+type LoginRes struct {
+	Res
+	OpenID     string `json:"openid"`
+	SessionKey string `json:"session_key"`
+	UnionID    string `json:"unionid"`
+}
+
+func (c *Client) Login(code string) (LoginRes, error) {
+
+	url := fmt.Sprintf(Host+"/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code", c.appId, c.secret, code)
+
+	var res GetUserInfoRes
+
+	_, err := gohttpclient.Get(url).Exec().RenderJSON(&res)
+
+	if err != nil {
+
+		return "", err
+	} else if res.Errcode != 0 {
+
+		return "", errors.New(res.Errmsg)
+
+	}
+
+	return res, nil
+
+}
