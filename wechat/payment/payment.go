ea168a876be0f5d4cae7778ea4af96f071c69866
luaxlou <luax@qq.com>
2020-02-28 08:58:42 +0800

    sign

--- wechat/payment/payment.go
+++ wechat/payment/payment.go
@@ -1,16 +1,11 @@
 package payment
 
 import (
-	"encoding/json"
 	"encoding/xml"
-	"io/ioutil"
-	"net/http"
+	"log"
 
 	"github.com/clbanning/mxj"
-	"github.com/gin-gonic/gin"
-	"github.com/labstack/gommon/log"
 	"github.com/liyoung1992/wechatpay"
-	"github.com/luaxlou/goutils/tools/logutils"
 )
 
 const (
@@ -44,55 +39,27 @@ func (c *Payment) Pay(tradeNo string, totalFee int, subject string, notifyUrl st
 	return c.client.Pay(pay_data)
 }
 
-func (w *Payment) HandleNotify(c *gin.Context, onSucc, onFail func(rawBody string)) {
+func (w *Payment) VerifyNotify(body []byte) (wechatpay.PayNotifyResult, bool) {
 
-	log.Info("wechat notify start")
-	body, err := ioutil.ReadAll(c.Request.Body)
+	var result wechatpay.PayNotifyResult
+
+	err := xml.Unmarshal(body, &result)
 	if err != nil {
-		log.Error(err, "read http body failed！error msg:"+err.Error())
-	}
-
-	rawBody := string(body)
-
-	log.Info("wechat pay notify body :" + rawBody)
-
-	var wx_notify_req wechatpay.PayNotifyResult
-
-	err = xml.Unmarshal(body, &wx_notify_req)
-	if err != nil {
-		log.Error(err, "read http body xml failed! err :"+err.Error())
+		log.Println("read http body xml failed! err :" + err.Error())
 	}
 	mv, err := mxj.NewMapXml(body)
 	if err != nil {
-		log.Error(err, err.Error())
+		log.Println(err.Error())
 	}
 
 	if w.client.VerifySign(mv["xml"].(map[string]interface{}), mv["xml"].(map[string]interface{})["sign"].(string)) {
-		record, err := json.Marshal(wx_notify_req)
 
-		onSucc(rawBody)
-
-		logutils.PrintObj(record)
-		if err != nil {
-			log.Error(err, "wechat pay marshal err :"+err.Error())
-		}
-		c.XML(http.StatusOK, gin.H{
-			"return_code": "SUCCESS",
-			"return_msg":  "OK",
-		})
-	} else {
-
-		onFail(rawBody)
-
-		c.XML(http.StatusOK, gin.H{
-			"return_code": "FAIL",
-			"return_msg":  "failed to verify sign, please retry!",
-		})
+		return result, true
 	}
-	return
-}
 
-const PaySignTemplate = "appId=%s&nonceStr=%s&package=prepay_id=%s&signType=MD5&timeStamp=%d&key=%s"
+	return result, false
+
+}
 
 // 小程序 客户端唤起支付签名
 func (c *Payment) GetPaySign(nonceStr string, prepayId string, ts string) string {
