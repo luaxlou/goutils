a55f380de3da27ee81ca8633a3569a78a813d92d
luaxlou <luax@qq.com>
2020-03-14 10:17:44 +0800

    add mysql db

---
+++ mysqldb/mysqldb.go
@@ -0,0 +1,61 @@
+package mysqldb
+
+import (
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+func New(dsn string) *gorm.DB {
+
+	if dsn == "" {
+
+		dsn = "root:123456@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=True&loc=Local&multiStatements=true"
+
+	}
+
+	return initDB(dsn)
+
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
+
+func Close() {
+
+	if db != nil {
+		db.Close()
+
+	}
+
+}
+
+func initDB(dsn string) (db *gorm.DB) {
+
+	if dsn == "" {
+		panic("dsn must not empty")
+
+	}
+
+	db, err := gorm.Open("mysql", dsn)
+
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(200)
+	db.DB().SetConnMaxLifetime(3 * 60 * time.Second)
+
+	if err != nil {
+		panic("mysql connect error " + err.Error())
+
+	}
+
+	if db.Error != nil {
+		panic("database error " + db.Error.Error())
+
+	}
+
+	return db
+}
---
+++ mysqldb/mysqldb_test.go
@@ -0,0 +1,9 @@
+package mysqldb
+
+import "testing"
+
+func TestNew(t *testing.T) {
+
+	dsn := "root:123456@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=True&loc=Local&multiStatements=true"
+	New(dsn)
+}
