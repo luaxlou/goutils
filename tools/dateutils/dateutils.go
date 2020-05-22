1dbedce962e1141d9c2952a73b8479c79cfaacac
luaxlou <luax@qq.com>
2020-05-22 20:04:41 +0800

    sliceutils

--- tools/dateutils/dateutils.go
+++ tools/dateutils/dateutils.go
@@ -3,6 +3,7 @@ package dateutils
 import "time"
 
 const STANARD_FORMAT = "2006-01-02 15:04:05"
+const DATE_FORMAT = "2006-01-02"
 const FLAT_FORMAT = "20060102150405"
 
 func StandardFormat(ts int64) string {
---
+++ tools/sliceutils/sliceutils.go
@@ -0,0 +1,14 @@
+package sliceutils
+
+func ContainsString(arr []string, str string) bool {
+
+	for _, s := range arr {
+
+		if s == str {
+			return true
+		}
+	}
+
+	return false
+
+}
