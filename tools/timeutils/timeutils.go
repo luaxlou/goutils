1131775c13f78aab174b96628802f8601a437886
luaxlou <luax@qq.com>
2020-09-17 11:33:53 +0800

    add delayTick

--- tools/timeutils/timeutils.go
+++ tools/timeutils/timeutils.go
@@ -20,3 +20,20 @@ func ImmediatelyTick(execution func(), duration time.Duration) {
 	}()
 
 }
+func DelayTick(execution func(), duration time.Duration) {
+
+	t := time.NewTicker(duration)
+
+
+	go func() {
+		for {
+			<-t.C
+
+			execution()
+
+		}
+
+		t.Stop()
+	}()
+
+}
