9a1a3906727ed48ef33c0b915d6e4b88f9a2db53
luaxlou <luax@qq.com>
2020-10-26 11:34:45 +0800

    fix ismobile bug

--- tools/validationutils/validationutils_test.go
+++ tools/validationutils/validationutils_test.go
@@ -1,1 +1,13 @@
 package validationutils
+
+import (
+	"testing"
+
+	"github.com/luaxlou/goutils/tools/logutils"
+)
+
+func TestIsMobile(t *testing.T) {
+	mobile :="16600000000"
+
+	logutils.PrintObj(IsMobile(mobile))
+}
