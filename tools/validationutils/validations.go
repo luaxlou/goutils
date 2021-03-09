db5625e3060586e7b15356c56354717240e162b2
luaxlou <luax@qq.com>
2021-03-09 11:09:23 +0800

    is mobile

--- tools/validationutils/validations.go
+++ tools/validationutils/validations.go
@@ -6,7 +6,7 @@ import (
 
 //Copy from https://blog.csdn.net/qq_38572383/article/details/85776570
 func IsMobile(str string) bool {
-	reg := `^1([38][0-9]|14[579]|5[^4]|6[6]|7[1-35-8]|9[189])\d{8}$`
+	reg := `^1([38][0-9]|14[579]|5[^4]|6[6]|7[01-35-8]|9[189])\d{8}$`
 	rgx := regexp.MustCompile(reg)
 
 	return rgx.MatchString(str)
