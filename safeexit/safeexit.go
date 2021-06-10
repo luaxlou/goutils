592207df69487790e63903eacd40fc6edcf268f2
luaxlou <luax@qq.com>
2021-06-10 13:37:12 +0800

    better safe exit

--- safeexit/safeexit.go
+++ safeexit/safeexit.go
@@ -10,14 +10,18 @@ import (
 func Capture(beforeExit func()) {
 	c := make(chan os.Signal)
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGUSR1, syscall.SIGUSR2)
-	log.Println("应用已启动")
 
-	for s := range c {
-		switch s {
-		case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM:
-			log.Println("收到退出信号:", s)
-			beforeExit()
-			os.Exit(0)
+	go func() {
+		for s := range c {
+			switch s {
+			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM:
+				log.Println(s,"waiting for exit..." )
+				beforeExit()
+				log.Println(s,"exited." )
+
+				os.Exit(0)
+			}
 		}
-	}
+	}()
+
 }
