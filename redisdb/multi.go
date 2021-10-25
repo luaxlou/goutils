5e54244f48257fe87d13a433fa3a0ca2925dd2c0
楼律江 <luax@qq.com>
2021-10-25 16:50:31 +0800

    redis增加多例模式

---
+++ redisdb/multi.go
@@ -0,0 +1,33 @@
+package redisdb
+
+import (
+	"github.com/go-redis/redis"
+	"sync"
+)
+
+//使用多例模式======
+
+var instances sync.Map
+
+func Add(name, host string, pass string, db int) {
+
+	_, ok := instances.Load(name)
+
+	if ok {
+		return
+	}
+
+	instances.Store(name, New(host, pass, db))
+
+}
+
+func GetInstance(name string) *redis.Client {
+
+	ins, ok := instances.Load(name)
+
+	if ok {
+		return ins.(*redis.Client)
+	}
+
+	return nil
+}
